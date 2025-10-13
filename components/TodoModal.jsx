3edad72af324fc4c477f3ec4ae928b7a83bbdbed
Erik Behrends <behrends@users.noreply.github.com>
2025-10-13 12:41:53 +0200

    Komponente für Todo-Modal hinzugefügt

---
+++ components/TodoModal.jsx
@@ -0,0 +1,53 @@
+import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+
+export default function TodoModal({ visible }) {
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.text}>Todo hinzufügen</Text>
+          <View style={styles.buttons}>
+            <Button title="Abbrechen" onPress={() => {}} />
+            <Button title="Speichern" onPress={() => {}} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  content: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+});
