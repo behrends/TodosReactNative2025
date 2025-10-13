bd880cd02dd6eb861a45d982d43cad7213450581
Erik Behrends <behrends@users.noreply.github.com>
2025-10-13 11:45:58 +0200

    Todo als Objekte mit id im lokalen Array

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Todo from './components/Todo';
 
-const todos = ['Einkaufen', 'Sport', 'React Native lernen'];
+const todos = [
+  { id: 1, text: 'Einkaufen' },
+  { id: 2, text: 'Sport' },
+  { id: 3, text: 'React Native lernen' },
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {todos.map((todo, index) => (
-        <Todo key={index}>{todo}</Todo>
+      {todos.map((todo) => (
+        <Todo key={todo.id}>{todo.text}</Todo>
       ))}
       <StatusBar style="auto" />
     </View>
