83fcb9657b3b40cdf301235065e1803066c9e6a8
Erik Behrends <behrends@users.noreply.github.com>
2025-10-13 12:03:43 +0200

    TodoList mit Styles und Trennstrich

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,14 +1,26 @@
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import Todo from './Todo';
 
 export default function TodoList({ todos }) {
   return (
     <FlatList
+      style={styles.list}
       data={todos}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => (
         <Todo key={item.id}>{item.text}</Todo>
       )}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  list: {
+    width: '100%',
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: 'darkgrey',
+  },
+});
