8bd22ac893ad19869fe4704d104f73de99eff8fc
Erik Behrends <behrends@users.noreply.github.com>
2025-10-13 11:42:03 +0200

    Props destrukturieren

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-function Todo(props) {
-  const { children } = props;
+function Todo({ children }) {
   return <Text style={styles.todoText}>{children}</Text>;
 }
 
